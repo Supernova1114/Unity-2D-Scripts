1ca12c4eb2c2dc2553decbd66fb814c5d90d454f
Cameron Rosenthal <supernovawarriors128@gmail.com>
2022-10-04 03:23:00 -0700

    Fixed ScriptableMonsterSpawner

--- Fighting Game/Assets/ScriptableMonsterSpawner.cs
+++ Fighting Game/Assets/ScriptableMonsterSpawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 /*
  * Scripting language for ScriptableMonsterSpawner.
- * s: spawn command.
- * p: position command.
- * t: delay time command.
+ * s: spawn command. value: prefabList index.
+ * p: position command. value: spawnPoint list index.
+ * t: delay time command. value: time to delay (in seconds)
  * 
- * ex: p2;t1;s3;t1;s3;p0;t1;s5;
+ * ex: p2;t1;s3;t1.1;s3;p0;t1;s5;
  */
 
 public class ScriptableMonsterSpawner : MonoBehaviour
@@ -16,77 +16,103 @@ public class ScriptableMonsterSpawner : MonoBehaviour
     [SerializeField] private GameObject[] prefabList;
     [SerializeField] private Transform[] spawnPoints;
 
-    private int currentSpawnPointIndex = 0;
-
     [SerializeField] private string scriptString;
+    [SerializeField] private bool runOnAwake;
+
+    private int currentSpawnPointIndex = 0;
 
     private void Start()
     {
+        if (runOnAwake)
+        {
+            PlayScript();
+        }
+    }
+
+    /// <summary>
+    /// Run given script now.
+    /// </summary>
+    public void PlayScript(string script)
+    {
+        SetScript(script);
         PlayScript();
     }
 
 
+    /// <summary>
+    /// Run previously set script.
+    /// </summary>
     public void PlayScript()
     {
-        IEnumerator ScriptCoroutine()
-        {
-            string scriptSubString;
-            char command;
-            float value;
-            int index;
-
-           
-            index = 0;
-
-            // p2;t1;s3;t1;s3;p0;t1;s5;
-            // index 
-            //
-            //
-            //
-            //
-            //
-
-            while (index < scriptString.Length)
-            {
-                // Get command. ex: p, t, s
-                command = scriptString[index];
-                index++;
-
-                print("Command: " + command);
-
-                // Get value. ex: 1.1, 5, 0
-                string valueString = scriptString.Substring(index, Mathf.Abs(scriptString.IndexOf(';') - index));
-                value = float.Parse(valueString);
-
-                index += valueString.Length + 1;
-
-                print("ValueStr: " + valueString);
-
-                // Run command using specified value as argument.
-                switch (command)
-                {
-                    case 'p':
-                        // Choose spawn position
-                        currentSpawnPointIndex = (int)value;
-                        break;
-                    case 't':
-                        // Delay time
-                        yield return new WaitForSeconds(value);
-                        break;
-                    case 's':
-                        // Spawn something
-                        Instantiate(prefabList[(int)value], spawnPoints[currentSpawnPointIndex].position, spawnPoints[currentSpawnPointIndex].rotation);
-                        break;
-                }
-            }
-
-            yield return null;
-        }
-
         StartCoroutine(ScriptCoroutine());
     }
 
-    
+
+    /// <summary>
+    /// Coroutine for running the script.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ScriptCoroutine()
+    {
+        string subString;
+        char command;
+        float value;
+        int index;
+
+        subString = scriptString;
+        index = 0;
+
+        // Parse and run script
+        while (index < scriptString.Length)
+        {
+            // Get command. ex: p, t, s
+            command = subString[0];
+
+            // Get value. ex: 1.1, 5, 0
+            string valueString = subString.Substring(1, subString.IndexOf(';') - 1);
+            value = float.Parse(valueString);
+
+            int increment = valueString.Length + 2;
+
+            // Remove parsed command.
+            subString = subString.Substring(increment);
+            index += increment;
+
+            // Run command using specified value as argument.
+            switch (command)
+            {
+                case 'p':
+                    // Choose spawn position
+                    currentSpawnPointIndex = (int)value;
+                    break;
+                case 't':
+                    // Delay time
+                    yield return new WaitForSeconds(value);
+                    break;
+                case 's':
+                    // Spawn something
+                    Instantiate(prefabList[(int)value], spawnPoints[currentSpawnPointIndex].position, spawnPoints[currentSpawnPointIndex].rotation);
+                    break;
+            }
+        }
+
+        yield return null;
+    }
+
+
+    /// <summary>
+    /// Stop script from running.
+    /// </summary>
+    public void StopScript()
+    {
+        StopCoroutine(ScriptCoroutine());
+    }
+
+
+    /// <summary>
+    /// Set script string.
+    /// </summary>
+    /// <param name="scriptString"></param>
     public void SetScript(string scriptString)
     {
         this.scriptString = scriptString;
