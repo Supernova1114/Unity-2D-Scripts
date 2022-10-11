f75be858453ef3d005945fda191e001a171b7384
Cameron Rosenthal <supernovawarriors128@gmail.com>
2022-10-11 11:54:55 -0700

    Added infinite ammo to ranged weapons

--- Fighting Game/Assets/RangedWeapon.cs
+++ Fighting Game/Assets/RangedWeapon.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public abstract class RangedWeapon : Weapon
 {
     [SerializeField] private int maxAmmo;
+    [SerializeField] private bool infiniteAmmo;
     [SerializeField] protected Bullet bullet;
 
     private int currentAmmo;
@@ -42,7 +43,11 @@ public abstract class RangedWeapon : Weapon
     /// </summary>
     protected override void OnAttack()
     {
-        if (currentAmmo > 0)
+        if (infiniteAmmo)
+        {
+            OnOnAttack();
+        }
+        else if (currentAmmo > 0)
         {
             currentAmmo--;
             OnOnAttack();
